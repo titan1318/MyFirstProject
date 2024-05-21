57ea4db09c4c6edc1313f5c6d14700cbbaada99a
titan1318 <vidok_91@mail.ru>
2024-05-21 12:50:59 +0300

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,6 +1,6 @@
 def uppercase_string(input_string):
     """
-    Принимает строку и возвращает ее в верхнем регистре.
+    Принимает строку и возвращает её в верхнем регистре.
 
     Args:
         input_string (str): Входная строка.
@@ -9,3 +9,15 @@ def uppercase_string(input_string):
         str: Строка в верхнем регистре.
     """
     return input_string.upper()
+
+def capitalize_words(input_string):
+    """
+    Принимает строку и делает заглавными первые буквы каждого слова.
+
+    Args:
+        input_string (str): Входная строка.
+
+    Returns:
+        str: Строка с заглавными первыми буквами каждого слова.
+    """
+    return input_string.title()
