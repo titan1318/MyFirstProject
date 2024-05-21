ae797be8aa9df7be4730e1d135b91df6e4b2df92
titan <vidok_91@mail.ru>
2024-05-21 12:31:41 +0300

    Added str_func.py with uppercase_string function

---
+++ str_func.py
@@ -0,0 +1,11 @@
+def uppercase_string(input_string):
+    """
+    Принимает строку и возвращает ее в верхнем регистре.
+
+    Args:
+        input_string (str): Входная строка.
+
+    Returns:
+        str: Строка в верхнем регистре.
+    """
+    return input_string.upper()
